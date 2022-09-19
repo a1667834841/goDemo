720d7ff1ebd07c10b436881cb6d725327c2c27b4
ggball <1667834841@qq.com>
2022-09-19 19:34:19 +0800

    指针 study

--- pointer/v3/wallet_test.go
+++ pointer/v3/wallet_test.go
@@ -1,6 +1,7 @@
 package pointer
 
 import (
+	"errors"
 	"fmt"
 	"testing"
 )
@@ -29,10 +30,21 @@ type Integer interface {
 	Int() int
 }
 
+var InsufficientFundsError = errors.New("cannot withdraw, insufficient funds")
+
 //  func (wallet Wallet) 修改为 func (wallet *Wallet) 你可以将其解读为「指向 wallet 的指针」。
 func (wallet *Wallet) Deposit(monry Bitcoin) {
 	fmt.Println("deposit address of balance in test is", &wallet.balance)
 	wallet.balance += monry
+}
+
+// 取钱
+func (wallet *Wallet) Withdraw(money Bitcoin) error {
+	if money > wallet.balance {
+		return InsufficientFundsError
+	}
+	wallet.balance -= money
+	return nil
 }
 
 func (wallet *Wallet) Balance() Bitcoin {
@@ -40,16 +52,56 @@ func (wallet *Wallet) Balance() Bitcoin {
 }
 
 func TestWallet(t *testing.T) {
-	wallet := Wallet{}
-	// 存款10
-	wallet.Deposit(10)
 
-	got := wallet.Balance()
-	want := Bitcoin(10)
+	assertBalance := func(t *testing.T, wallet Wallet, want Bitcoin) {
+		got := wallet.Balance()
 
-	fmt.Println("mian address of balance in test is", &wallet.balance)
+		if got != want {
+			t.Errorf("got %d want %d", got, want)
+		}
+	}
 
-	if got != want { // 比较内存地址
-		t.Errorf("got %v, want %v", got, want)
+	t.Run("Deposit", func(t *testing.T) {
+		wallet := Wallet{}
+		// 存款10
+		wallet.Deposit(10)
+
+		want := Bitcoin(10)
+
+		fmt.Println("mian address of balance in test is", &wallet.balance)
+
+		assertBalance(t, wallet, want)
+	})
+
+	t.Run("Withdraw", func(t *testing.T) {
+		wallet := Wallet{balance: Bitcoin(20)}
+
+		wallet.Withdraw(10)
+
+		want := Bitcoin(10)
+
+		assertBalance(t, wallet, want)
+	})
+
+	t.Run("Withdraw insufficient funds", func(t *testing.T) {
+		wallet := Wallet{balance: Bitcoin(20)}
+
+		got := wallet.Withdraw(200)
+
+		assertBalance(t, wallet, Bitcoin(20))
+
+		assertError(t, got, InsufficientFundsError)
+
+	})
+
+}
+
+func assertError(t *testing.T, got error, want error) {
+	if got == nil {
+		t.Fatal("didn't get an error but wanted one")
+	}
+
+	if got != want {
+		t.Errorf("got '%s', want '%s'", got, want)
 	}
 }
