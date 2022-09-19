dac8cc22651fff313933703609cf7f5549539c98
ggball <1667834841@qq.com>
2022-09-19 19:34:24 +0800

    指针 study

--- pointer/v2/wallet_test.go
+++ pointer/v2/wallet_test.go
@@ -13,6 +13,11 @@ type Wallet struct {
 	balance int
 }
 
+// 当调用 func (wallet Wallet) Deposit(amount int) 时，wallet 是来自我们调用方法的副本。
+// 运行结果
+// address of balance in Deposit is 0xc420012268
+// address of balance in test is 0xc420012260
+
 //  func (wallet Wallet) 修改为 func (wallet *Wallet) 你可以将其解读为「指向 wallet 的指针」。
 func (wallet *Wallet) Deposit(monry int) {
 	fmt.Println("deposit address of balance in test is", &wallet.balance)
